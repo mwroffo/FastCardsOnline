f5f736ae1738892afbc128ec99a5bf5af355719d
Michael <mwroffo@gmail.com>
2018-08-07 16:29:15 -0400

    added venv

--- app/forms.py
+++ app/forms.py
@@ -6,23 +6,25 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     # DataRequired makes the form refuse empty fields.
-    username = StringField('Username', validators=[DataRequired(), ])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log In')
-
 
 class EntryForm(FlaskForm):
     mwroffo = DecksModel('mwroffo')
     decks = mwroffo.getDecks()
     # create a list of value, label pairs:
-    choices = []
-    for deckname in decks.keys():  # populate it:
+    choices = [] # declare choices:
+    for deckname in decks.keys():  # populate choices.
         choices.append((deckname, deckname.upper()))
-    deck = SelectField(
+    # define fields:
+    selectedDeck = SelectField(
         'Choose a deck: ',
-        choices=[choices]
+        choices=choices,
+        validators=[DataRequired()]
     )
-    term = StringField('term', validators=[DataRequired()])
-    definition = StringField('definition', validators=[DataRequired()])
+    term = StringField('Term', validators=[DataRequired()])
+    definition = StringField('Definition', validators=[DataRequired()])
+    # dont forget submit button:
     submit = SubmitField('Add card to deck')
--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,11 @@ def index():
     # TODO this should prob be done with url_for()
     mwroffo = DecksModel('/Users/_mexus/Documents/code/fastcardsonline/mwroffo')
     decks = mwroffo.getDecks()
-    return render_template('index.html', title='Home',
-                           user=user, decks=decks.keys())
+    return render_template(
+        'index.html',
+        title='Home',
+        user=user,
+        decks=decks.keys())
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -27,12 +30,16 @@ def login():
         return redirect(url_for('index.html'))
     return render_template('login.html', title='Sign In to FastCards', form=form, user={'username': 'mwroffo'})
 
-
-@app.route('/entry', methods=['GET', 'POST'])
-def entry():
+@app.route('/browse_edit', methods=['GET', 'POST'])
+def browse_edit():
     form = EntryForm()
     if form.validate_on_submit():
         flash('Card saved: term={}, definition={}'.format(
             form.term.data, form.definition.data))
-        return redirect(url_for('entry'))
-    return render_template('entry.html', title='Enter a card', form=form)
+        return redirect(url_for('browse_edit.html'))
+    return render_template('browse_edit.html', title='Enter a card',  user={'username': 'mwroffo'}, form=form)
+
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    form = EntryForm() # TODO 2018-08-05 change from placeholder to ReviewForm()
+    return render_template('review.html', title='Reviewing deck', form=form, user={'username': 'mwroffo'})
