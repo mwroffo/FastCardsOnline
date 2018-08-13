7ce72681d29d923a806d585f5fb94fe68dd9314d
Michael <mwroffo@gmail.com>
2018-08-13 15:51:12 -0400

    removed superfluous language in config.py

--- config.py
+++ config.py
@@ -5,6 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sirius black'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    # database changes according to user logged in? would this cause problems
-    # when multiple users are signed in?
     SQLALCHEMY_TRACK_MODIFICATIONS = False
