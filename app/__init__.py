69c1c936008ecd7ecb7e729d920c12850f3ca59c
Michael <mwroffo@gmail.com>
2018-08-14 19:05:04 -0400

    testing against microblog tutorial files suggests that the lack of emails in my fake python email server has little to do with bad code on my part. moving on.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, Blueprint
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,13 +11,30 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
-    from . import db
     db.init_app(app) # configures app's teardown_context and cli
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    ### register email error logging:
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'],
+                        app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='FastCards Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
 
     return app
 
--- config.py
+++ config.py
@@ -5,4 +5,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sirius black'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # suppresses a deprecation warning
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # suppres
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['mwroffo@gmail.com']
