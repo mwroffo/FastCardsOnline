0301581708d5f6d0d189c758d6ec607ef1357872
Michael <mwroffo@gmail.com>
2018-08-18 16:00:57 -0400

    functional response tests are GO!

--- tests/test_fastcards.py
+++ tests/test_fastcards.py
@@ -1,29 +1,56 @@
-import os
-import tempfile
+import os, tempfile, pytest
+from app import create_app, db
+from config import TestingConfig, DevelopmentConfig
+from app.models import User, Card
 import flask
-import sys
-sys.path.insert(0 '/Users/_mexus/Documents/code/FastCardsOnline/app')
-import app.forms
+import pytest
 
-import pytest, os, flask, tempfile
-from flask import current_app, g
+@pytest.fixture(scope='module')
+def test_client():
+    """
+    Initializes a client for a suite of functional tests for FastCards.
+    """
+    app = create_app(DevelopmentConfig) # init the flask instance
 
-@pytest.fixture
-def client():
-    """ Called by each individual test """
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
+    # init werkzeug test client from flask instance. this will be used in functional tests.
     client = app.test_client()
 
-    with app.app_context():
-        app.init_db()
+    # an application context enables the flask instance to respond to GET and POST requests.
+    app_context = app.app_context()
+    app_context.push() # binds the current app to this app_context
 
     yield client
+    # in pytest, everything after the yield statement serves as teardown:
+    app_context.pop() # pop the context after each test.
 
-    os.close(db_fd)
-    os.unlink(app.app.config['DATABASE'])
+@pytest.fixture(scope='module')
+def init_db():
+    db.create_all()
+    u = User(username='billy', email='billy-bob@gmail.com')
+    u2 = User(username='andrew', email='andrew-dude@comcastsucks.net')
+    u.set_password('real_password')
+    u2.set_password('that-is-an-L-bro')
+    db.session.add(u)
+    db.session.add(u2)
+    db.session.commit()
+    yield db    # TODO must admit I don't understand how yield is applicable, pytest.
+                # what does this fixture have in common with generators?
+    db.drop_all()
 
-def test_request(client):
-    current_app = flask.Flask(__name__)
-    deck_form = DeckForm()
-    # with app.test_request_context('/browse_edit', data=data)
+def test_login_redirect(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested via GET
+    THEN check that the response is a 302 redirect.
+    """
+    response = test_client.get('/')
+    assert response.status_code == 302
+
+def test_login_200(test_client):
+    """
+    GIVEN a Flask app
+    WHEN the '/login' page is requested via GET
+    THEN check that the response is 200
+    """
+    response = test_client.get('/login')
+    assert response.status_code == 200
